9565270072ec5ef3ed286703d603a2e2a28ba65b
fds <fran.mejias.7e5@itb.cat>
2021-12-13 20:28:06 +0100

    JavaScript

--- IntentoJavaScript/Intento1/JavaScript/javaScript.js
+++ IntentoJavaScript/Intento1/JavaScript/javaScript.js
@@ -1,10 +1,37 @@
-var f1Button = document.querySelector("#ChColorFirtsButton");
-var f1Text = document.querySelector("#ChColorFirtsText");
+var f1Text = document.querySelector("#ChColorF1Text");
+var f1Button = document.querySelector("#ChColorF1Button");
+var reload = document.querySelectorAll(".reloadw");
 
 f1Button.addEventListener("click",chColor);
 
 function chColor() {
 
-    f1Text.style.background = "red";
+    f1Text.style.color = "red";
+
+}
+
+reload[1].addEventListener("click",reloader);
+
+function reloader(){
+
+    location.reload(reload);
+
+}
+
+
+function create() {
+    // createElement
+    var elementNode = document.createElement('h1');
+    // createTextNode
+    var textNode = document.createTextNode('Saludos');
+
+    elementNode.appendChild(textNode);
+    document.body.appendChild(elementNode);
+}
+
+
+function remover() {
+
+    f1Text.parentNode.removeChild(f1Text);
 
 }
