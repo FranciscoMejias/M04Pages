241cc8efcc4f0ea3cb713934a52277def4a03bd3
fds <fran.mejias.7e5@itb.cat>
2021-12-13 19:03:37 +0100

    JavaScript

---
+++ IntentoJavaScript/Intento1/JavaScript/javaScript.js
@@ -0,0 +1,10 @@
+var f1Button = document.querySelector("#ChColorFirtsButton");
+var f1Text = document.querySelector("#ChColorFirtsText");
+
+f1Button.addEventListener("click",chColor);
+
+function chColor() {
+
+    f1Text.style.background = "red";
+
+}
